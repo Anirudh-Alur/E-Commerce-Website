2011339033d21d52e059f75d8ae67693f0fda4f8
Anirudh Alur <anirudhalur99@gmail.com>
2025-06-25 20:21:05 +0530

    Fix: correct settings module path

--- ecomsite/manage.py
+++ ecomsite/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomsite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomsite.ecomsite.settings')
+   
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
