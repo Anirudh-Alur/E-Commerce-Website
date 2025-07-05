ce753165da06bb1dcf7393eb540e29863b0ae475
Anirudh Alur <anirudhalur99@gmail.com>
2025-07-06 00:14:47 +0530

    new pagination

--- shop/views.py
+++ shop/views.py
@@ -18,6 +18,7 @@ def index(request):
         product_objects = product_objects.filter(title__icontains=item_name)
 
     # Pagination
+    products = Products.objects.all().order_by('id')
     paginator = Paginator(product_objects, 4)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
